3b8d14dacc9bc5abe48ecb8e739e2143bf6a1999
Safa Arıman <safa@ariman.gen.tr>
2019-08-31 01:18:10 +0300

    update cache version #15

--- pisi/db/lazydb.py
+++ pisi/db/lazydb.py
@@ -38,7 +38,7 @@ class Singleton(object):
 
 class LazyDB(Singleton):
 
-    cache_version = "2.4"
+    cache_version = "3.0.0"
 
     def __init__(self, cacheable=False, cachedir=None):
         if "initialized" not in self.__dict__:
